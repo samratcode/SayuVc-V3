9f7cc5c56d6c2d1bb0ffea94fe32f2766b456381
S <85238140+samratcode@users.noreply.github.com>
2021-11-14 03:46:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ then
   git clone https://github.com/samratcode/SayuVc-V3 /SayuVc-V3
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/samratcode/SayuVc-V3 -b $BRANCH /VCPlayerBot
+  git clone https://github.com/samratcode/SayuVc-V3 -b $BRANCH /SayuVc-V3
 fi
 cd /SayuVc-V3
 pip3 install -U -r requirements.txt
