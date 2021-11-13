8f4ccb06f18a06aa9c39d7dca9429ba822263c2e
S <85238140+samratcode@users.noreply.github.com>
2021-11-14 04:22:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -439,4 +439,4 @@ For more info on channel play , read help from player section.__
 18. `FPS` : __Fps of video to be played (Not recommended to change.)__
 
 """
-    ADMIN_ONLY=is_enabled(os.environ.get("ADMIN_ONLY", str("False"))
+    ADMIN_ONLY=is_enabled(os.environ.get("ADMIN_ONLY", "False"))
